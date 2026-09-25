416585278bf1c39d9e914dbd090f0595f96f0871
sunkyongmin <37209520+sunkyongmin@users.noreply.github.com>
2026-09-25 09:43:16 +0900

    set PATH for cron git push

--- backup_snapshots.sh
+++ backup_snapshots.sh
@@ -1,4 +1,5 @@
 #!/bin/zsh
+export PATH=/usr/bin:/bin:/usr/local/bin:/opt/homebrew/bin:$PATH
 cd ~/bigdata/ed-transfer || exit 1
 STAMP=$(date +%Y%m%d_%H%M)
 mkdir -p ~/Library/Mobile\ Documents/com~apple~CloudDocs/ed-transfer-snapshots
